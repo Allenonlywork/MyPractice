4b43f344502555bcb8fe07f118d6b00f89c3c86e
Allen <xieq1747@163.com>
2019-11-21 11:10:00 +0800

    add comment server is not be use

--- src/com/redis_call/RedisJava.java
+++ src/com/redis_call/RedisJava.java
@@ -6,6 +6,7 @@ public class RedisJava {
 
     public static void main(String[] args) {
         //连接远程服务器的 Redis 服务
+        //服务器到期停用了 oh my god
         Jedis jedis = new Jedis("106.54.78.122");
         System.out.println("连接成功");
         //查看服务是否运行
