831f77c44dc8918c2406d16d99242247af7a5e32
Allen <xieq1747@163.com>
2019-12-04 21:52:57 +0800

    算法题练习 1、斐波那契数列 2、100-200之间的素数

---
+++ src/algorithm/oneTo10/Fibonacci.java
@@ -0,0 +1,23 @@
+package algorithm.oneTo10;
+
+/**
+ * 古典问题：有一对兔子，从出生后第3个月起每个月都生一对兔子，小兔子长到第三个月后每个月又生一对兔子，假如兔子都不死，问每个月的兔子总数为多少？
+ */
+public class Fibonacci {
+    /**
+     * @Author xieqiong
+     * @param args
+     */
+    public static void main(String[] args) {
+        int f1 = 1,f2 = 1,f;
+        int M = 30;
+        System.out.println(f1);
+        System.out.println(f2);
+        for(int i=3;i<M;i++){
+            f = f2;
+            f2 = f1 + f2;
+            f1 = f2;
+            System.out.println(f2);
+        }
+    }
+}
