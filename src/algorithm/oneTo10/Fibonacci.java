b8f0f9868c3720ceb0c0a858dd2414fdab943e62
Allen <xieq1747@163.com>
2019-12-05 15:21:23 +0800

    算法题练习 1、2 注释更新

--- src/algorithm/oneTo10/Fibonacci.java
+++ src/algorithm/oneTo10/Fibonacci.java
@@ -1,12 +1,15 @@
 package algorithm.oneTo10;
 
 /**
+ * @Description
  * 古典问题：有一对兔子，从出生后第3个月起每个月都生一对兔子，小兔子长到第三个月后每个月又生一对兔子，假如兔子都不死，问每个月的兔子总数为多少？
  */
 public class Fibonacci {
     /**
      * @Author xieqiong
      * @param args
+     * @Version 1.0
+     * @DATE 20191204
      */
     public static void main(String[] args) {
         int f1 = 1,f2 = 1,f;
--- src/algorithm/oneTo10/PrimeNumber.java
+++ src/algorithm/oneTo10/PrimeNumber.java
@@ -1,10 +1,17 @@
 package algorithm.oneTo10;
 
 /**
+ * @Description
  * 题目：判断101-200之间有多少个素数，并输出所有素数。
  * 程序分析：判断素数的方法：用一个数分别去除2到sqrt(这个数)，如果能被整除， 则表明此数不是素数，反之是素数。
  */
 public class PrimeNumber {
+    /**
+     * @Author xieqiong
+     * @param args
+     * @Version 1.0
+     * @DATE 20191204
+     */
     public static void main(String[] args) {
         int count = 0;
         for(int i = 101;i < 200;i += 2){
