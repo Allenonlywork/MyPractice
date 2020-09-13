12105bdb419d30381e8ca284b30e739dae52d9c9
Allen <xieq1747@163.com>
2020-09-13 13:58:42 +0800

    找重复字符并排序输出

---
+++ src/com/daily_job/_202008/_20200912p/Solution1.java
@@ -0,0 +1,22 @@
+package com.daily_job._202008._20200912p;
+
+import java.util.Scanner;
+
+/**
+ * 题目描述：找出输入字符串中的重复字符，再根据ASCII码把重复的字符从小到大排序。
+ */
+public class Solution1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int[] arr = new int[150];
+        for(int i = 0;i< s.length();i++){
+            arr[s.charAt(i)] ++;
+        }
+        for(int j =0;j<arr.length;j++){
+            if(arr[j] > 1){
+                System.out.print((char)j);
+            }
+        }
+    }
+}
