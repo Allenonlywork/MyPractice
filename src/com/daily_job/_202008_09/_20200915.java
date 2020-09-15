2912f634edeba0c8af0e3bcac317e35764e4b45a
Allen <xieq1747@163.com>
2020-09-15 22:23:43 +0800

    数字自动补零

---
+++ src/com/daily_job/_202008_09/_20200915.java
@@ -0,0 +1,10 @@
+package com.daily_job._202008_09;
+
+public class _20200915 {
+    public static void main(String[] args) {
+//        System.out.println(String.format("%0" + 4 + "d",1));
+        for(int i = 1;i < 25;i++){
+            System.out.println(String.format("%0" + 4 + "d",i));
+        }
+    }
+}
